9ce23c76e69b44ec54af40750d9c6885b5ec1e8a
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-05-18 23:23:07 -0300

    fix bug 2

--- src/utils/canSSRGuest.ts
+++ src/utils/canSSRGuest.ts
@@ -1,20 +1,19 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
 
-export function canSSRGuest<P>(fn: GetServerSideProps<P>) {
+export function canSSRGuest<P extends { [key: string]: any }>(fn: GetServerSideProps<P>) {
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
-
         const cookies = parseCookies(ctx);
 
-        if(cookies['@nextauth.token']) {
+        if (cookies['@nextauth.token']) {
             return {
                 redirect: {
                     destination: '/dashboard',
                     permanent: false
                 }
-            }
+            };
         }
 
         return await fn(ctx);
-    }
+    };
 }
